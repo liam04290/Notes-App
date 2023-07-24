2932d3dda511482e512dbe59b4f27968ef1e0a9e
Liam Robertson <liam0429@gmail.com>
2023-07-24 09:55:48 -0400

    Heroku testing

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
 app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.post('/api/notes', (req, res) => {
