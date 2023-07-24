c832ef3e27277f40c042272a756e37278a004a02
Liam Robertson <liam0429@gmail.com>
2023-07-24 08:45:53 -0400

    Added server.js functionality

---
+++ server.js
@@ -0,0 +1,47 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+// Setup Express.js App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile('db/db.json', JSON.stringify(notes, null, 2), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
